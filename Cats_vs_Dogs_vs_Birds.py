719d5ac1deca587911daeb94f1415a958272f395
Nnaemeka <nwankwonnaemeka21@gmail.com>
2024-01-19 03:15:25 +0100

    Moved file
    Created a new function

--- Cats_vs_Dogs_vs_Birds.py
+++ Cats_vs_Dogs_vs_Birds.py
@@ -9,7 +9,7 @@ import seaborn as sns
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-
+from utils import *
 
 # To ignore some warnings about Image metadata that Pillow prints out
 import warnings
@@ -19,7 +19,7 @@ cats_vs_dogs_dir = 'kagglecatsanddogs_3367a.zip'
 caltech_birds_dir = 'CUB_200_2011.tar'
 
 BASE_DIR = './tmp/data'
-#os.makedirs(BASE_DIR)
+os.makedirs(BASE_DIR)
 
 # unzips the dataset
 with zipfile.ZipFile(cats_vs_dogs_dir, 'r') as zip:
@@ -59,3 +59,30 @@ plt.show()
 #image = mpimg.imread(os.path.join(base_dogs_dir, os.listdir(base_dogs_dir)[0]))
 #plt.imshow(image)
 #plt.show()
+
+
+train_eval_dirs = ['train/cats', 'train/dogs', 'train/birds',
+                   'eval/cats', 'eval/dogs', 'eval/birds']
+
+for dir in train_eval_dirs:
+  if not os.path.exists(os.path.join(BASE_DIR, dir)):
+    os.makedirs(os.path.join(BASE_DIR, dir))
+
+base_dogs_dir = os.path.join(BASE_DIR, 'PetImages/Dog')
+base_cats_dir = os.path.join(BASE_DIR,'PetImages/Cat')
+base_birds_dir = os.path.join(BASE_DIR,'PetImages/Bird')
+
+# Move 80% of the images to the train dir
+move_to_destination(base_cats_dir, os.path.join(BASE_DIR, 'train/cats'), 0.8)
+move_to_destination(base_dogs_dir, os.path.join(BASE_DIR, 'train/dogs'), 0.8)
+move_to_destination(base_birds_dir, os.path.join(BASE_DIR, 'train/birds'), 0.8)
+
+
+# Move the remaining images to the eval dir
+move_to_destination(base_cats_dir, os.path.join(BASE_DIR, 'eval/cats'), 1)
+move_to_destination(base_dogs_dir, os.path.join(BASE_DIR, 'eval/dogs'), 1)
+move_to_destination(base_birds_dir, os.path.join(BASE_DIR, 'eval/birds'), 1)
+
+# Sanity check 
+print(len(os.listdir(os.path.join(BASE_DIR, 'train/birds'))))
+print(len(os.listdir(os.path.join(BASE_DIR, 'eval/birds'))))
